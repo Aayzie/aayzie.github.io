7ba74a45fe8f33486e2dfe93c038315d7cc37d0f
forsensgiftbox <aayzie@gmail.com>
2022-08-07 16:43:14 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+answers = "";
+
+curSection = 0;
+sections = [
+    "menu",
+    "intelligence",
+    "intelligencea",
+    "intelligenceb",
+    "intelligencec",
+    "intelligenced",
+    "intelligencee",
+    "decisionmaking",
+    "decisionmakinga",
+    "decisionmakingb",
+    "decisionmakingc",
+    "decisionmakingd",
+    "decisionmakinge",
+    "decisionmakingf",
+    "decisionmakingg",
+    "decisionmakingh",
+    "decisionmakingi",
+    "decisionmakingj",
+    "decisionmakingk",
+    "decisionmakingl",
+    "opinions",
+    "opinionsa",
+    "opinionsb",
+    "opinionsc",
+    "opinionsd",
+    "opinionse",
+    "opinionsf",
+    "opinionsg",
+    "opinionsh",
+    "situations",
+    "situationsa",
+    "situationsb",
+    "situationsc",
+    "situationsd",
+    "situationse",
+    "end"
+]
+
+showSection("menu");
+
+function showSection(sectionName)
+{
+    document.getElementById(sectionName).style.display = "block";
+}
+function hideSection(sectionName)
+{
+    document.getElementById(sectionName).style.display = "none";
+}
+
+function start()
+{
+    goToNextSection();
+    var backgroundMusic = new Audio('everytime.mp3');
+    backgroundMusic.play();
+}
+
+function goToNextSection()
+{
+    hideSection(sections[curSection]);
+    curSection++;
+    showSection(sections[curSection]);
+    if(sections[curSection] == "end")
+    {
+        showResults();
+    }
+}
+
+function showResults()
+{
+    document.getElementById("results").value = answers.slice(0, -1);
+}
+
+function answer(answerLetter)
+{
+    answers += answerLetter + ",";
+    console.log(answers);
+    goToNextSection();
+    console.log(sections[curSection]);
+}
